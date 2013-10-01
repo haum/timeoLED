8dcba7abd9efda0cfc6f5ab0206f80b62a9558a5
jerome B <jerome@monocycle.(none)>
2013-10-01 18:11:47 +0200

    initial commit

---
+++ timeoLED.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# -*- coding:utf8 -*-
+#
+# timeoLED.py
+#
+# Copyright © 2013 jerome B (jblbl) <jerome@jblb.no-ip.org>
+#
+#
+# Distributed under WTFPL terms
+#
+# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+# Version 2, December 2004
+#
+# Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+# TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+# 0. You just DO WHAT THE FUCK YOU WANT TO.
+#
+ 
+"""
+  
+"""
+import requests
+import re
+# from bs4 import BeautifulSoup as BS
+
+
+URL="http://timeoapi.haum.org"
+session = requests.Session()
+
+session.headers.update({'User-Agent': 'timeoLED', 'Content-type': 'application/x-www-form-urlencoded'})
+
+session.get(URL)
+
+get_lines = "/v1/lines"
+
+result=session.get(URL+get_lines)                        
